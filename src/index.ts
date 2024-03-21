09b0707490e5ed0fc3f10766240b648d6a63009d
kuscoder <kuscoder@yandex.ru>
2024-03-21 21:24:22 +0300

    chore: update package minor version

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import '@/assets/chart.scss'
 export * from '@/lib/chart'
 export * from '@/lib/chart-error'
 export type * from '@/lib/types'
-// TODO: The ability to add types for x Axis other than "date"
+// TODO: The ability to add types for timeline other than "date"
 // TODO: Debug mode
 // TODO: Controls and hints in example
 // TODO: Vector2 util
-// TODO: Rename x\yAxis-like vars and methods
 // TODO: tooltip
